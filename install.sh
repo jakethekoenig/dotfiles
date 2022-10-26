2710c581cbf9f34b6ca4348742d0ad4b370651f7
jaek <jakethekoenig@gmail.com>
2022-10-26 09:26:08 -0700

    Not sure what temp was for

--- install.sh
+++ install.sh
@@ -5,12 +5,10 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo ~
 echo $CURRENT_DIR
 
-mkdir temp
 rm -rf ~/.vim
 rm ~/.vimrc
 ln -s "$CURRENT_DIR/vim" ~/.vim
 ln -s "$CURRENT_DIR/vim/vimrc" ~/.vimrc
-rm -rf temp
 
 rm ~/.zshrc
 rm -r ~/.zsh
